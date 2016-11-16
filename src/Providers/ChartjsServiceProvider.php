dec307cd78fae233f7ae9dfbce829fb813bbb0a5
laggards <laggards@hotmail.com>
2016-11-16 10:08:48 +0800

    daily commit

---
+++ src/Providers/ChartjsServiceProvider.php
@@ -0,0 +1,54 @@
+<?php namespace Laggards\laravel-chartjs\Providers;
+
+use Laggards\laravel-chartjs\ChartBar;
+use Laggards\laravel-chartjs\ChartLine;
+use Laggards\laravel-chartjs\ChartPieAndDoughnut;
+use Laggards\laravel-chartjs\ChartPolar;
+use Laggards\laravel-chartjs\ChartRadar;
+use Illuminate\Support\ServiceProvider;
+
+class ChartjsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Array with colours configuration of the chartjs config file
+     * @var array
+     */
+    protected $colours = [];
+
+    public function boot()
+    {
+        $this->publishes([__DIR__.'/../config/chartjs.php' => config_path('chartjs.php')]);
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'autoload');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'chart-bar');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'chart-line');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'chart-radar');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'chart-pie-doughnut');
+        $this->colours = config('chartjs.colours');
+    }
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('chartbar', function() {
+            return new ChartBar($this->colours['bar']);
+        });
+
+        $this->app->bind('chartline', function() {
+            return new ChartLine($this->colours['line']);
+        });
+
+        $this->app->bind('chartpiedoughnut', function() {
+            return new ChartPieAndDoughnut($this->colours['pie']);
+        });
+
+        $this->app->bind('chartradar', function() {
+            return new ChartRadar($this->colours['radar']);
+        });
+    }
+}
